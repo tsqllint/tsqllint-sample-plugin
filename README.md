0e5b1f1613e3d1870ae6cea7af1cb5fbcaf8937f
Nathan Boyd <nboyd@salesforce.com>
2021-08-26 11:10:26 -0400

    add sample plugin project

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# tsqllint-sample-plugin
+# TSQLLint Sample Plugin
+
+This repo contains a sample solution and project which builds a TSQLLint plugin.
+
+## Usage
+
+Build the plugin and pu
+
+```
+dotnet build tsqllint-sample-plugin.sln --output /tmp/tsqllint-plugin
+```
+
+Add a plugin object to your tsqllint config, usually located at `~/.tsqllintrc`
+
+```
+    "plugins": {
+      "sample-plugin":"/tmp/tsqllint-plugin/tsqllint-sample-plugin.dll"
+    }
+```
+
+Test the plugin
+
+```bash
+echo "   select 1;" > ~/Desktop/foo.sql && tsqllint ~/Desktop/foo.sql
+```
+
+You should see a message like the following
+
+```
+~/Desktop/foo.sql(1,-1): warning prefer-tabs : Should use spaces rather than tabs.
+```
---
+++ tsqllint-sample-plugin/SamplePlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using TSQLLint.Common;
+using System.IO;
+
+namespace TSQLLint.Tests.UnitTests.PluginHandler
+{
+    public class SamplePlugin : IPlugin
+    {
+        public void PerformAction(IPluginContext context, IReporter reporter)
+        {
+            string line;
+            var lineNumber = 0;
+
+			var reader = new StreamReader(File.OpenRead(context.FilePath));
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                lineNumber++;
+                var column = line.IndexOf("\t", StringComparison.Ordinal);
+                reporter.ReportViolation(new SampleRuleViolation(
+                    context.FilePath,
+                    "prefer-tabs",
+                    "Should use spaces rather than tabs",
+                    lineNumber,
+                    column,
+                    RuleViolationSeverity.Warning));
+            }
+        }
+    }
+
+    public class SampleRuleViolation : IRuleViolation
+    {
+        public int Column { get; private set; }
+        public string FileName { get; private set; }
+        public int Line { get; private set; }
+        public string RuleName { get; private set; }
+        public RuleViolationSeverity Severity { get; private set; }
+        public string Text { get; private set; }
+
+        public SampleRuleViolation(string fileName, string ruleName, string text, int lineNumber, int column, RuleViolationSeverity ruleViolationSeverity)
+        {
+            FileName = fileName;
+            RuleName = ruleName;
+            Text = text;
+            Line = lineNumber;
+            Column = column;
+            Severity = ruleViolationSeverity;
+        }
+    }
+}
